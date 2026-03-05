c5b8c93ce14a0bc2bf5b664fc06ce3304531a8c5
a-neun <153486449+eunhye-ahn@users.noreply.github.com>
2026-03-05 14:27:07 +0900

    260305_자바시작

---
+++ Chapter01_자바시작/src/자바_구동.java
@@ -0,0 +1,24 @@
+/*
+ * 		JDK 구성
+ * 		---------
+ * 		JRE => 실행환경
+ * 		---------
+ * 		JVM -> Devlopment Tool => bin
+ * 			-> javac : 컴퓨터가 인식하는 언어로 변경
+ * 			-> java : 프로그램 실행 => 화면 출력
+ * 						한줄씩 읽어서 출력 (인터프리터(HTML/CSS/JS))
+ * 			-> javap : 역컴파일러
+ * 			-> javaw : 서버구동 => 톰캣 (웹서버)
+ * 			-> jar	 : 	배포 => zip
+ */
+
+
+public class 자바_구동 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("<html><body><h1>Hello JAVA</h1></body></html>");
+		
+	}
+
+}
---
+++ Chapter01_자바시작/src/자바_시작.java
@@ -0,0 +1,25 @@
+/*
+ * 		.java => javac => .class(byteCode) => JVM => OS
+ * 
+ * 		import => 미국식 => 변경해서 사용 -------- 오버라이딩
+ * 		
+ * 		package 
+ * 		import
+ * 		public class {
+ * 			static void main(String[] args) {
+				// TODO Auto-generated method stub
+
+			}
+ * 		}
+ * 
+ */
+
+
+public class 자바_시작 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
